ebb690382ab532cdd7d334bcf30d5798ef4b90e6
na2sime <na2s.contact@gmail.com>
2026-06-24 15:02:46 +0200

    fix: RDS SSL (sslmode=require) + rabbitmq tcp probes (diagnostics too slow)

--- infra/terraform/secrets.tf
+++ infra/terraform/secrets.tf
@@ -21,7 +21,7 @@ resource "aws_secretsmanager_secret" "app" {
 resource "aws_secretsmanager_secret_version" "app" {
   secret_id = aws_secretsmanager_secret.app.id
   secret_string = jsonencode({
-    DATABASE_URL      = "postgres://${var.db_username}:${random_password.db.result}@${module.rds.db_instance_endpoint}/${var.db_name}"
+    DATABASE_URL      = "postgres://${var.db_username}:${random_password.db.result}@${module.rds.db_instance_endpoint}/${var.db_name}?sslmode=require"
     JWT_SECRET        = random_password.jwt.result
     RABBITMQ_URL      = "amqp://m2cloud:${random_password.rabbitmq.result}@rabbitmq.app.svc.cluster.local:5672"
     RABBITMQ_PASSWORD = random_password.rabbitmq.result
